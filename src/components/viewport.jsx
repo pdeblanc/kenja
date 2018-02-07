2f2d1fc69bb22363b57b10cb832c2d4d932acad7
Peter de Blanc <spaceandgames@gmail.com>
2018-02-07 02:16:05 -0800

    Render SVG viewport.

--- src/components/viewport.jsx
+++ src/components/viewport.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 
 import {rangeMap} from '../util/array';
 
-export default ({plane}) => {
-  return (<div>
+export default ({plane, x=10, y=10, radius=4}) => {
+  return (
+    <svg
+      width='100%'
+      height='100%'
+      viewBox={[-radius - 0.5, -radius - 0.5, 2 * radius + 1, 2 * radius + 1].join(' ')}
+    >
       {
-        rangeMap(0, 10, y => (
-          <div key={y}>
-            {
-              rangeMap(0, 10, x => (
-                <span key={x}>
-                  {plane.get(x, y).symbol}
-                </span>
-              ))
-            }
-          </div>
-        ))
+        rangeMap(- radius, radius + 1, v =>
+          rangeMap(-radius, radius + 1, u =>
+            <text x={u} y={v} fill='black' key={[u,v]} style={{fontSize: '1px'}}>{plane.get(x + u, y + v).symbol}</text>
+          )
+        )
       }
-    </div>
+    </svg>
   );
 }
