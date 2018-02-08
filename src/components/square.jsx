af18891b75c6216aa8f42c1f11768e167a03ab52
Peter de Blanc <spaceandgames@gmail.com>
2018-02-07 22:49:36 -0800

    Add colored squares for terrain.

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 export default ({x, y, biome}) => (
-  <g className={biome.name} transform={`translate(${x}, ${y})`}>
-    <text fill='black' style={{fontSize: '1px'}}>{biome.symbol}</text>
+  <g className={'biome ' + biome.name} transform={`translate(${x}, ${y})`}>
+    <rect x="-0.5" y="-0.5" width="1" height="1" />
+    <text y="0.37">{biome.symbol}</text>
   </g>
 );
