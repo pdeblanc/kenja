4591ae9b80f070e9914e191483a076b78f6eed1e
Peter de Blanc <spaceandgames@gmail.com>
2018-02-07 23:04:36 -0800

    Show outline on hovered square.

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 export default ({x, y, biome}) => (
-  <g className={'biome ' + biome.name} transform={`translate(${x}, ${y})`}>
+  <svg
+    className={'biome ' + biome.name}
+    x={x - 0.5}
+    y={y - 0.5}
+    width="1px"
+    height="1px"
+    viewBox="-0.5 -0.5 1 1"
+  >
     <rect x="-0.5" y="-0.5" width="1" height="1" />
     <text y="0.37">{biome.symbol}</text>
-  </g>
+  </svg>
 );
