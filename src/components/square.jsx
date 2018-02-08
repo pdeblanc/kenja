dbbd5e88dca926d831bf1a624b44368c0201c813
Peter de Blanc <spaceandgames@gmail.com>
2018-02-07 23:24:12 -0800

    Add mouse navigation.

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default ({x, y, biome}) => (
+export default ({x, y, biome, click}) => (
   <svg
     className={'biome ' + biome.name}
     x={x - 0.5}
@@ -8,6 +8,7 @@ export default ({x, y, biome}) => (
     width="1px"
     height="1px"
     viewBox="-0.5 -0.5 1 1"
+    onClick={click}
   >
     <rect x="-0.5" y="-0.5" width="1" height="1" />
     <text y="0.37">{biome.symbol}</text>
--- src/components/viewport.jsx
+++ src/components/viewport.jsx
@@ -3,22 +3,44 @@ import React from 'react';
 import Square from './square.jsx';
 import {rangeMap} from '../util/array';
 
-export default ({plane, x=10, y=10, radius=4}) => {
-  return (
-    <div>
+class Foo extends React.Component {
+  constructor(props) {
+    super(props);
+    const {x, y} = this.props;
+    this.state = {x, y};
+  }
+
+  render() {
+    const {x, y} = this.state;
+    const {plane, radius} = this.props;
+    return <div>
       <svg
         width='100%'
         height='100%'
         viewBox={[-radius - 0.5, -radius - 0.5, 2 * radius + 1, 2 * radius + 1].join(' ')}
       >
         {
-          rangeMap(- radius, radius + 1, v =>
+          rangeMap(-radius, radius + 1, v =>
             rangeMap(-radius, radius + 1, u =>
-              <Square key={[u, v]} x={u} y={v} biome={plane.get(x + u, y + v)} />
+              <Square
+                key={[u, v]}
+                x={u}
+                y={v}
+                biome={plane.get(x + u, y + v)}
+                click={() => this.setState({x: x + u, y: y + v})}
+              />
             )
           )
         }
       </svg>
     </div>
-  );
+  }
 }
+
+Foo.defaultProps = {
+  x: 10,
+  y: 10,
+  radius: 4
+};
+
+export default Foo;
