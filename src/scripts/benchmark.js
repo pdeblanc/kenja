49714b27f40bde7053911ad14c2f42de32a8a14f
Peter de Blanc <spaceandgames@gmail.com>
2018-02-08 22:26:31 -0800

    Benchmark sha hashing from hash.js.

--- src/scripts/benchmark.js
+++ src/scripts/benchmark.js
@@ -1,13 +1,22 @@
 import {logOfGamma11} from '../util/random';
+import sha1 from 'hash.js/lib/hash/sha/1';
+import sha224 from 'hash.js/lib/hash/sha/224';
+import sha256 from 'hash.js/lib/hash/sha/256';
+import sha512 from 'hash.js/lib/hash/sha/512';
 
-function benchmark(name, callback) {
+function benchmark(name, callback, calls=1000000) {
   const startTime = Date.now();
-  for (let i = 0; i < 1000000; i++) {
+  for (let i = 0; i < calls; i++) {
     callback();
   }
   const endTime = Date.now();
-  console.log(name + ': ' + (endTime - startTime) + ' ns per call');
+  const nsPerCall = (endTime - startTime) * 1000000 / calls;
+  console.log(name + ': ' + nsPerCall + ' ns per call');
 }
 
 benchmark('Math.random', Math.random);
 benchmark('logOfGamma11', logOfGamma11);
+benchmark('sha1', () => sha1().update('abc').digest('hex'), 10000);
+benchmark('sha224', () => sha224().update('abc').digest('hex'), 10000);
+benchmark('sha256', () => sha256().update('abc').digest('hex'), 10000);
+benchmark('sha512', () => sha512().update('abc').digest('hex'), 10000);
