fd213227fa5e2797da6943ccd253bf257f42dfe8
Peter de Blanc <spaceandgames@gmail.com>
2018-02-08 23:53:16 -0800

    Benchmark more random stuff.

--- src/scripts/benchmark.js
+++ src/scripts/benchmark.js
@@ -1,10 +1,12 @@
-import {logOfGamma11} from '../util/random';
 import md5 from 'md5';
 import murmurhash from 'murmurhash';
 import sha1 from 'hash.js/lib/hash/sha/1';
 import sha224 from 'hash.js/lib/hash/sha/224';
 import sha256 from 'hash.js/lib/hash/sha/256';
 import sha512 from 'hash.js/lib/hash/sha/512';
+import gaussian from 'gaussian';
+import {rnorm} from 'randgen';
+import {soranzoEpureCdf, soranzoEpurePpf} from '../util/random';
 
 function benchmark(name, callback, calls=1000000) {
   const startTime = Date.now();
@@ -16,12 +18,23 @@ function benchmark(name, callback, calls=1000000) {
   console.log(name + ': ' + nsPerCall + ' ns per call');
 }
 
+const standardGaussian = gaussian(0, 1);
+
 benchmark('Math.random', Math.random);
-benchmark('logOfGamma11', logOfGamma11);
 benchmark('murmurhash.v2', () => murmurhash.v2('foo'));
 benchmark('murmurhash.v3', () => murmurhash.v3('foo'));
+benchmark('standardGaussian.cdf', () => standardGaussian.cdf(2));
+benchmark('standardGaussian.ppf', () => standardGaussian.ppf(0.95));
+benchmark('soranzoEpureCdf', () => soranzoEpureCdf(2));
+benchmark('soranzoEpurePpf', () => soranzoEpurePpf(0.95));
+benchmark('rnorm', () => rnorm(0, 1));
 benchmark('md5', () => md5('foo'), 10000);
 benchmark('sha1', () => sha1().update('abc').digest('hex'), 10000);
 benchmark('sha224', () => sha224().update('abc').digest('hex'), 10000);
 benchmark('sha256', () => sha256().update('abc').digest('hex'), 10000);
 benchmark('sha512', () => sha512().update('abc').digest('hex'), 10000);
+
+console.log('standardGaussian.cdf(2) == ' + standardGaussian.cdf(2))
+console.log('soranzoEpureCdf(2) == ' + soranzoEpureCdf(2))
+console.log('standardGaussian.ppf(0.977) == ' + standardGaussian.ppf(0.977))
+console.log('soranzoEpurePpf(0.977) == ' + soranzoEpurePpf(0.977))
--- src/util/random.js
+++ src/util/random.js
@@ -1,7 +1,22 @@
 import {min} from './array';
+import {v2} from 'murmurhash';
 
-// log of a random variable following Gamma(1, 1) distribution
-export const logOfGamma11 = () => Math.log(-Math.log(1 - Math.random()));
+// uniform distribution on [0, 1]
+export const uniform = (seedStr, seedInt) => v2(seedStr, seedInt) / 2 ** 32;
+
+// exponential distribution with λ = 1
+export const exponential = () => Math.log(-Math.log(1 - Math.random()));
 
 // sample from a finite set with given energy function
-export const dobbins = (arr, nrg) => min(arr, x => nrg(x) + logOfGamma11());
+export const dobbins = (arr, nrg) => min(arr, x => nrg(x) + exponential());
+
+// http://m-hikari.com/ams/ams-2014/ams-85-88-2014/epureAMS85-88-2014.pdf
+export const soranzoEpureCdf = x => 2 ** (-(22 ** (1 - 41 ** (x / 10))));
+
+const c1 = 10 / Math.log(41);
+const c2 = 1 / Math.log(22);
+const c3 = -1 / Math.log(2);
+
+export const soranzoEpurePpf = α => c1 * Math.log(
+  1 - c2 * Math.log(c3 * Math.log(α))
+);
