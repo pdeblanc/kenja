2f3f48d4d69e6d1d2567828af35ca5eb7428baa6
Peter de Blanc <spaceandgames@gmail.com>
2018-02-16 23:01:03 -0800

    Add woods; change page title to 'World Simulator'.

--- src/defs/biomes.js
+++ src/defs/biomes.js
@@ -2,6 +2,7 @@ import Biome from '../world/biome.js';
 
 export default [
   new Biome('grass', '草', ({height, temperature}) => -height + temperature ** 2 - 3),
+  new Biome('woods', '林', ({height, temperature}) => -height + temperature ** 2 - 3),
   new Biome('sand', '砂', ({height}) => -height),
   new Biome('water', '水', ({height}) => height),
   new Biome('wall', '壁', ({x, y}) => {
