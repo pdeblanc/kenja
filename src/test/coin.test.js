8963394de62b453f18c8e349e501b0106e475c7c
Peter de Blanc <spaceandgames@gmail.com>
2018-08-12 17:08:23 -0700

    Split up test cases for CoinCache.achieveDepth.

--- src/test/coin.test.js
+++ src/test/coin.test.js
@@ -1,26 +1,30 @@
 import {choose} from '../util/numeric';
 import {clearAll, CoinCache} from '../distributions/coin';
 
-test('CoinCache(1/2) precomputes correct triangle up to n=20', () => {
+describe('CoinCache(1/2).achieveDepth(20)', () => {
   clearAll();
   const cache = new CoinCache(1/2);
   cache.achieveDepth(20);
   for (let n = 0; n <= 20; n++) {
     for (let k = 0; k <= n; k++) {
-      expect(cache.pmf.get(n, k)).toBe(choose(n, k) / 2 ** n);
+      const desired = choose(n, k) / 2 ** n;
+      test(`pmf should be ${desired} for n=${n}, k=${k}`, () => {
+        expect(cache.pmf.get(n, k)).toBe(desired);
+      })
     }
   }
 });
 
-test('CoinCache(1/4) precomputes correct triangle up to n=16', () => {
+describe('CoinCache(1/4).achieveDepth(16)', () => {
   clearAll();
   const cache = new CoinCache(1/4);
   cache.achieveDepth(16);
   for (let n = 0; n <= 16; n++) {
     for (let k = 0; k <= n; k++) {
-      expect(cache.pmf.get(n, k)).toBe(
-        choose(n, k) * 0.25 ** k * 0.75 ** (n - k)
-      );
+      const desired = choose(n, k) * 0.25 ** k * 0.75 ** (n - k)
+      test(`pmf should be ${desired} for n=${n}, k=${k}`, () => {
+        expect(cache.pmf.get(n, k)).toBe(desired);
+      })
     }
   }
 });
@@ -32,7 +36,7 @@ describe('new CoinCache(1/4).fromLeft(n, k)', () => {
     cache.fromLeft(n, n);
     for (let k = 0; k <= n; k++) {
       const desired = choose(n, k) * 0.25 ** k * 0.75 ** (n - k)
-      test(`pmf should yield ${desired} for n=${n}, k=${k}`, () => {
+      test(`pmf should be ${desired} for n=${n}, k=${k}`, () => {
         expect(cache.pmf.get(n, k)).toBe(desired);
       })
     }
@@ -46,7 +50,7 @@ describe('new CoinCache(1/4).fromRight(n, k)', () => {
     cache.fromRight(n, 0);
     for (let k = 0; k <= n; k++) {
       const desired = choose(n, k) * 0.25 ** k * 0.75 ** (n - k)
-      test(`pmf should yield ${desired} for n=${n}, k=${k}`, () => {
+      test(`pmf should be ${desired} for n=${n}, k=${k}`, () => {
         expect(cache.pmf.get(n, k)).toBe(desired);
       })
     }
